d007492dd5e18c61faa21fbbb50797d6965dbc85
Aleix Oraa <aoraa1@iesebre.com>
2024-03-13 17:33:44 +0100

    video 10 acabat

--- resources/views/videos/show.blade.php
+++ resources/views/videos/show.blade.php
@@ -1,8 +1,18 @@
 <x-casteaching-layout>
-    <p>{{$video->title}}</p>
-    <ul>
-        <li>Description: {{$video->description}}</li>
-        <li>Data: {{$video->published_at->format('F d') }}</li>
-    </ul>
+    <div class="flex flex-col">
+        <iframe
+            class="md:p-3 lg:p-8 xl:px-10 xl:py-10 2xl:px-20 2xl:py-10"
+            height="600"
+            src="https://www.youtube.com/embed/ednlsVl-NHA?si=s1leGz3WGJARFrHO"
+            title="YouTube video player" frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen></iframe>
+        <div class="p-6 lg:p-8 xl:p-10 2xl:p-20">
+            {{$video->title}}
+        </div>
+        <div class="p-6 lg:p-8 xl:p-10 2xl:p-20">
+            {{$video->description}}
+        </div>
+    </div>
 </x-casteaching-layout>
 
