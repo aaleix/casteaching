ac13a5f5f7e2e52d271ed8a229ad744c216c0283
Aleix Oraa <aoraa1@iesebre.com>
2024-05-22 16:42:44 +0200

    formulari afegir videos

--- resources/views/videos/manage/index.blade.php
+++ resources/views/videos/manage/index.blade.php
@@ -1,15 +1,45 @@
 <x-casteaching-layout>
     <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
         @can('videos_manage_create')
-        <form data-qa="form_video_create" action="" method="POST">
-            <label for="title">Title</label>
-            <input id="title" name="title" type="text">
-            <label for="description">Description</label>
-            <textarea name="description" id="description" cols="30" rows="10"></textarea>
-            <label for="url">URL</label>
-            <input id="url" name="url" type="text">
-            <button>Crear</button>
-        </form>
+            <div class="space-y-10 divide-y divide-gray-900/10">
+                <div class="grid grid-cols-1 gap-x-8 gap-y-8 md:grid-cols-3 p-6">
+                    <div class="px-4 sm:px-0">
+                        <h2 class="text-base font-semibold leading-7 text-gray-900">Vídeos</h2>
+                        <p class="mt-1 text-sm leading-6 text-gray-600">Informació bàsica del vídeo</p>
+                    </div>
+
+                    <form class="bg-white shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl md:col-span-2">
+                        <div class="px-4 py-6 sm:p-8">
+                            <div class="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                                <div class="col-span-full">
+                                    <label for="title" class="block text-sm font-medium leading-6 text-gray-900">Title</label>
+                                    <div class="mt-1">
+                                        <input id="title" name="title" type="text" class="w-full rounded-md shadow-sm focus:ring-indigo-500 focus:border-gray-300">
+                                    </div>
+                                </div>
+                                <div class="col-span-full">
+                                    <label for="description" class="block text-sm font-medium leading-6 text-gray-900">Description</label>
+                                    <div class="mt-2">
+                                        <textarea id="description" name="about" rows="3" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+                                    </div>
+                                </div>
+                                <div class="sm:col-span-4">
+                                    <label for="url" class="block text-sm font-medium leading-6 text-gray-900">URL</label>
+                                    <div class="mt-2">
+                                        <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                                            <span class="flex select-none items-center pl-3 text-gray-500 sm:text-sm">http://</span>
+                                            <input type="text" name="url" id="url" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="www.example.com">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="flex items-center justify-end gap-x-6 border-t border-gray-900/10 px-4 py-4 sm:px-8">
+                            <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Crear</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         @endcan
         <div
             class="block rounded-lg bg-white p-6 text-surface shadow-secondary-1 dark:bg-surface-dark dark:text-white">
